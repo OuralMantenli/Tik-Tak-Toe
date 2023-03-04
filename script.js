b48fd68eb62478e367ecfb962d43caa3353d1e2d
Oural Mantenli <oural.mantenli@gmail.com>
2023-03-04 19:08:58 +0100

    disable double click on the field

--- script.js
+++ script.js
@@ -2,25 +2,28 @@ let fields = [];
 let currentShape = 'cross';
 
 function fillShape(id) {
-    if (currentShape == 'cross'){
-        currentShape = 'circle';
-        document.getElementById('player-2').classList.remove('player-inactive');
-        document.getElementById('player-1').classList.add('player-inactive');
-    } else {
-        currentShape = 'cross';
-        document.getElementById('player-1').classList.remove('player-inactive');
-        document.getElementById('player-2').classList.add('player-inactive');
+    if (!fields[id]) {
+        if (currentShape == 'cross') {
+            currentShape = 'circle';
+            document.getElementById('player-2').classList.remove('player-inactive');
+            document.getElementById('player-1').classList.add('player-inactive');
+        } else {
+            currentShape = 'cross';
+            document.getElementById('player-1').classList.remove('player-inactive');
+            document.getElementById('player-2').classList.add('player-inactive');
+        }
+        fields[id] = currentShape;
+        draw();
+        checkForWin();
     }
-    fields[id] = currentShape;
-    draw();
 }
 
 function draw() {
     for (let i = 0; i < fields.length; i++) {
-        if (fields[i]== 'circle') {
+        if (fields[i] == 'circle') {
             document.getElementById('circle-' + i).classList.remove('d-none');
         }
-        if (fields[i]== 'cross') {
+        if (fields[i] == 'cross') {
             document.getElementById('cross-' + i).classList.remove('d-none');
         }
     }
